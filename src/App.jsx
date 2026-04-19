a3ce18ca090b40134d80268daefd7113911f706e
gurunathanb-rgb <gurunathanb@gmail.com>
2026-04-19 08:15:46 +0530

    using class single javascript component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Header from "./Component/Header"
+import React from "react"
 
 
-function App() 
+class App extends React.Component
 {
+  render() {
   return (
   <>
-  <Header/>
   <h1>Hello Users</h1>  
   <p>Welcome to Axles India  web page</p>
   </>
   )
+}
 }
 
 export default App
