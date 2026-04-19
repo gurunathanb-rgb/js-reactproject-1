b339d8927d5849749bec9d1c4b23c14a54851f82
gurunathanb-rgb <gurunathanb@gmail.com>
2026-04-19 08:27:06 +0530

    using class multiple javascript component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Header from "./Component/Header"
 
 
 class App extends React.Component
@@ -6,6 +7,7 @@ class App extends React.Component
   render() {
   return (
   <>
+  <Header/>
   <h1>Hello Users</h1>  
   <p>Welcome to Axles India  web page</p>
   </>
---
+++ src/Component/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Header extends  React.Component {
+    render(){
+        return(
+            <>
+            <h1>Axles India Ltd</h1>
+            <h3> Using Class multiple Javascript component</h3>
+            <nav>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Contact us</li>
+            </nav>
+            </>
+        )
+    }
+
+}
+export default Header;
