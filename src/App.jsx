451dc916581bed001d80ca5448cb39f81441dbbe
gurunathanb-rgb <gurunathanb@gmail.com>
2026-04-19 12:03:35 +0530

    data from child to parent using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from "react"
-import Header from "./Component/Header"
+// parent component
+
+import Header from "./Component/Header";
 
 
-class App extends React.Component
-{
-  render() {
-  return (
-  <>
-  <Header/>
-  <h1>Hello Users</h1>  
-  <p>Welcome to Axles India  web page</p>
-  </>
-  )
+function App() {
+
+    // 1. create a function in the parent component
+    // to receive the data from the child component
+    const receiveChildData = (name, age) => {
+        console.log(`Hello ${name}! You are ${age} years old!`);
+    }
+
+    // 2. pass the function receiveChildData as props
+    return (
+        <>
+            <Header receiveChildData={receiveChildData} />
+        </>
+    )
 }
-}
 
-export default App
+export default App;
