eeef67ed0b08431804335e2223874362331d00a1
gurunathanb-rgb <gurunathanb@gmail.com>
2026-05-12 17:50:11 +0530

    adding an dependencies list  using UseEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const[count, setCount] = useState(0);
+  const[count, setCount] = useState(0)
+  const[count1,setCount1] = useState(0)
 
-  //With empty dependencies [] using UseEffect
+  //With  dependencies list [count] using UseEffect
   useEffect( () =>  {
 
         fetch('https://6a03077f0d92f63dd254c14e.mockapi.io/Employeedet')
@@ -11,13 +12,17 @@ const App = () => {
    .then(response => response.json())
    .then(Employeedet => console.log(Employeedet))
 
-  } , [])  // adding an Empty array in bottom of the UseEffect hook function
-
+  } , [count,count1])  // adding an dependencies once it changes it will render
+  //console.log(count1)
   
   return (
     <div>
       <button onClick={() => setCount(count+1)}>
       Fetch Employee Details
+      </button> &nbsp;
+
+      <button onClick={()=> setCount1(count1+1)}>
+        Fetch Employee Details -state
       </button>
       </div>
   )
