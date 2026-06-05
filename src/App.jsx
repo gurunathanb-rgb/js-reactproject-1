135e8ec1db6a90fed74e93ab5193e13e23e228d4
gurunathanb-rgb <gurunathanb@gmail.com>
2026-06-05 13:38:18 +0530

    Dynamic list rendering in html without map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,32 @@
-// controlled input: an input element whose value is controlled by React state.
-
-import { useState } from "react";
-
 const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const receipes = [
+    'Briyani',
+    'Pulao',
+    'Sambar rice',
+    'Curd Rice',
+    'Noodles',
+    'Sambar idli',
+    'Doza'
+  ]
+  //const list =[<li>Briyani</li>]
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    console.log('Logging in...');
-    console.log(email, password);
+  const list = []
+  receipes.forEach(receipe  => {
+    list.push(<li>{receipe}</li>)
 
-    // reset the form
-    setEmail("");
-    setPassword("");
+  })
 
-    event.target.email.focus();
-  }
-
+    
   return (
-    <div>
-      <form
-        onSubmit={handleLogin}
-        className="flex flex-col gap-4 w-100 mx-auto mt-10 border border-gray-300 rounded p-8 shadow-lg"
-      >
-        <h1
-          className="text-2xl font-bold text-center"
-        >Login</h1>
-        <div
-          className="flex flex-col gap-2"
-        >
-          <input
-            name="email"
-            type="email"
-            placeholder="Type your email..."
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-            className="border border-gray-300 rounded px-3 py-2"
-          />
-        </div>
+    <>
+      <h1>Receipes List</h1>
+      <ul>
+        {list}
+      </ul>
 
-        <div>
-          <input
-            name="password"
-            type="password"
-            placeholder="Type your password..."
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-            className="border border-gray-300 rounded px-3 py-2 w-full"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="bg-blue-500 text-white rounded px-3 py-2 w-full"
-        >
-          Login
-        </button>
-      </form>
-    </div>
+    </>
+    
   )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import React from 'react' // Good practice to include
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <div>
